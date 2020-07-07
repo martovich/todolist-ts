310253083b7ea4cbacfd1e8d05055dfef7f2a7f5
martovich <mart-1984@mail.ru>
2020-07-08 00:23:09 +0300

    todolist-ts lesson1

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 
-export function Todolist() {
+type TaskType = {
+    id: number,
+    title: string,
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string,
+    tasks: Array<TaskType>
+}
+export function Todolist(props: PropsType) {
     return (
         <div className="App">
             <div>
-                <h3>What to learn</h3>
+                <h3>{props.title}</h3>
                 <div>
                     <input/>
                     <button>+</button>
                 </div>
                 <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                    <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                    <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                    <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
                 </ul>
                 <div>
                     <button>All</button>
