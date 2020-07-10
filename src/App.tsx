2e14ad45957bcf86eaf12accc9c03b1ccb7aa6ae
martovich <mart-1984@mail.ru>
2020-07-10 22:02:14 +0300

    todolist-ts lesson3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
+import { v1 } from 'uuid';
 
 export type FilterValuesType = "all" | "completed" | "active";
 
 function App() {
     let [tasks, setTasks] = useState([
-        {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false},
-        {id: 4, title: "Rest API", isDone: true},
-        {id: 5, title: "GraphQL", isDone: false}
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "ReactJS", isDone: false},
+        {id: v1(), title: "Rest API", isDone: true},
+        {id: v1(), title: "GraphQL", isDone: false}
     ])
     let [filter, setFilter] = useState<FilterValuesType>("all");
 
