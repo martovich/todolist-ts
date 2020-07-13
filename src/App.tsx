7e5c9c0bec9cc222fad75a63e96a91ff45c011b8
martovich <mart-1984@mail.ru>
2020-07-13 16:58:20 +0300

    todolist-ts lesson3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
-import { v1 } from 'uuid';
+import {v1} from 'uuid';
 
 export type FilterValuesType = "all" | "completed" | "active";
 
@@ -28,9 +28,15 @@ function App() {
         setFilter(value);
     }
 
-    function removeTask(id: number) {
+    function removeTask(id: string) {
         let filterTasks = tasks.filter(t => t.id != id);
         setTasks(filterTasks);
+    }
+
+    function addTask(title: string) {
+        let task = {id: v1(), title: title, isDone: false};
+        let newTasks = [task, ...tasks];
+        setTasks(newTasks);
     }
 
     return (
@@ -38,7 +44,8 @@ function App() {
             <Todolist title="What to learn"
                       tasks={tasksForTodoList}
                       removeTask={removeTask}
-                      changeFilter={changeFilter}/>
+                      changeFilter={changeFilter}
+                      addTask={addTask}/>
         </div>
     );
 }
